c98372a04f5c2c509f905de5a6a428ac1ce95e12
SitiDwiWindaningsih <sitidwiwinda@gmail.com>
2021-04-11 08:39:06 +0700

    Siti Dwi Windaningsih

---
+++ app/src/main/java/com/example/fragmentappsti18d5/Firstfragment.java
@@ -0,0 +1,6 @@
+package com.example.fragmentappsti18d5;
+
+import android.app.Fragment;
+
+public class Firstfragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/fragmentappsti18d5/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fragmentappsti18d5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
